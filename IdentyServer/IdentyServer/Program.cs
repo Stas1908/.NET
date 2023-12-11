4e6240a6a49c715a6d268b0a2a68d972966c5c52
Stanislav Mintianskyi <134478219+Stas1908@users.noreply.github.com>
2023-12-11 08:35:17 +0200

    Add files via upload

---
+++ IdentyServer/IdentyServer/Program.cs
@@ -0,0 +1,13 @@
+using DataAcsess.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
